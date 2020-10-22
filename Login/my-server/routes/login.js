d6f060722c12e5f69f3d01ddb7251c7ce393456e
sdxy.ai <280411879@qq.com>
2020-10-22 19:38:13 +0800

    code

--- Login/my-server/routes/login.js
+++ Login/my-server/routes/login.js
@@ -1,5 +1,37 @@
 const router = require('koa-router')();
-const loginHandle = require('../routesHandle/login');
+const db = require('../db/index');
+
+
+const loginHandle = async (ctx, next) => {
+  const { username, password } = ctx.request.body;
+  console.log({ username, password })
+
+  try {
+    if (username.length >= 5) {
+      ctx.body = { code: 0, msg: '用户名错误' };
+      return;
+    } else if (password.length < 6) {
+      ctx.body = { code: 0, msg: '密码错误' };
+      return;
+    }
+    // 应该是后面多个括号
+    // 多条件查询的语句是这样的哈，有and/or之类的
+    const sql = `select count(*) as num from user where username="${username}" and password="${password}"`;
+    let result = await db.query(sql)
+    if (result[0].num) {
+      let result = { code: 1, msg: '登录成功' };
+      ctx.body = result;
+    } else {
+      let result = { code: 0, msg: '登录失败,用户名和密码不正确' };
+      ctx.body = result;
+    }
+  } catch (error) {
+    // 你看终端他报错了，所以走这个catch，这个catch没有给ctx.body赋值，所以404
+    console.log('[SELECT ERROR] - ', error.message)
+    // 这样其实也不太好
+    ctx.throw(500, '后台报错')
+  }
+}
 
 router.post('/api/login', loginHandle);
 
--- Login/my-server/routes/register.js
+++ Login/my-server/routes/register.js
@@ -1,5 +1,47 @@
 const router = require('koa-router')();
-const registerHandle = require('../routesHandle/register');
+const db = require('../db/index');
+const md5 = require('md5');
+
+const registerHandle = async (ctx, next) => {
+  const { nickname, email, password } = ctx.request.body;
+  console.log({ nickname, email, password });
+  // 目前的这个地方是ES6的简写，当属性名与变量名一致时可以这样写
+  // connection(sql, { username, email, password });
+  // 现在不一致了,就需要把对象的属性名和对象的属性值写全
+  try {
+    md5(password);
+    const sql1 = `select count(*) as num from user where username="${nickname}"`;
+    let result1 = await db.query(sql1)
+    if (result1[0].num) {
+      let result1 = { code: 0, msg: '用户名已存在' };
+      ctx.body = result1;
+    }
+    console.log(result1);
+    const sql2 = `select count(*) as num from user where email="${email}"`;
+
+    let result2 = await db.query(sql2)
+
+    if (result2[0].num) {
+      let result2 = { code: 0, msg: 'Email已存在' };
+      ctx.body = result2;
+    } else {
+      const sql = `insert  INTO  user(username,email,password)  values ('${nickname}','${email}','${password}')`;
+      let result = await db.query(sql)
+
+      if (result.affectedRows) {
+        let result = { code: 1, msg: '注册成功' };
+        ctx.body = result;
+      } else {
+        let result = { code: 0, msg: '注册失败' };
+        ctx.body = result;
+      }
+    }
+  } catch (error) {
+    console.log('[SELECT ERROR] - ', error.message)
+    ctx.throw(500, '后台报错')
+  }
+
+}
 
 router.post('/api/register', registerHandle);
 
